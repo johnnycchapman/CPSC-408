6b57ac0c2e4b7a97b5466b3d46afa89c8ce291f2
Johnny Chapman <chapm171@mail.chapman.edu>
2019-04-26 15:08:52 -0700

    Update DBConfig.java

--- assign3-cpsc408/src/DBConfig.java
+++ assign3-cpsc408/src/DBConfig.java
@@ -5,22 +5,7 @@ public class DBConfig {
 
     private Connection pgSqlConnection = null;
 
-    public static Connection getMyConnection()
-    {
-        Connection mysqlConnection = null;
-        try
-        {
-            //returns the Class object associated with the class or interface with the given
-            Class.forName("com.mysql.jdbc.Driver");
-            String connectionURL = "jdbc:mysql://35.233.129.12:3306/cpsc408";
-            mysqlConnection = DriverManager.getConnection(connectionURL, "johnnychapman", "Turkeyboy1");
-        }
-        catch(Exception ex)
-        {
-            ex.printStackTrace();
-        }
-        return mysqlConnection;
-    }
+
 
     public static Connection getMySqlConnection()
     {
